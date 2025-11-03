18db694b72bf7d97569a4e6e0c543820eb294e11
Ironjuan <juanjosearevalgaona@gmail.com>
2025-11-02 21:05:06 -0500

    Set NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloView/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloView from './view/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloView
   }
 }
 </script>
---
+++ src/view/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <AppNavbar/>
+        <h1>Hello from Hello.vue!</h1>
+    </div>
+
+</template>
+
+<script>
+
+import AppNavbar from '../components/Navbar.vue'
+
+export default {
+    name: 'HelloView',
+    components: {
+        AppNavbar
+    }
+}
+</script>
